290844b19f4bff0f2a97b631955111fe2c466b1d
akademia108 <kursy@akademia108.pl>
2020-05-17 17:04:34 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('test', () => {
+    console.log('Moje pierwsze zdanie GULP');
+});
+
+gulp.task('sass', () => {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
+});
+
+
+gulp.task('watch', () => {
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+});
